6ca0e7217879432b7303c6684c0dab508ae0c039
MoMutawe1 <mutalogix@gmail.com>
2022-01-24 21:05:21 -0600

    Implement Custom Finder Methods - Jan-24-2022

--- src/main/java/com/springdatarest/eventmanagementapi/repos/EventRepository.java
+++ src/main/java/com/springdatarest/eventmanagementapi/repos/EventRepository.java
@@ -1,9 +1,18 @@
 package com.springdatarest.eventmanagementapi.repos;
 
 import com.springdatarest.eventmanagementapi.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.ZoneId;
 
 @Repository
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+
+    Page<Event> findByName(@Param("name")String name, Pageable pageable);
+
+    Page<Event> findByNameAndZoneId(@Param("name")String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
 }
