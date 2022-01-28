ff29f0f5cad691737b1c9f57ff0e3c01cbeeb63d
MoMutawe1 <mutalogix@gmail.com>
2022-01-27 21:09:39 -0600

    Apply password encoding - Jan-27-2022

--- src/main/java/com/springdatarest/eventmanagementapi/SecurityConfiguration.java
+++ src/main/java/com/springdatarest/eventmanagementapi/SecurityConfiguration.java
@@ -11,14 +11,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Mohammad").password("MyPass").roles("USER")
-                .and().withUser("Meepo").password("MepoPass").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("Mohammad").password("{noop}MyPass").roles("USER")
+                .and().withUser("Meepo").password("{noop}MepoPass").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.POST,"/events").hasRole("ADMIN").
                 antMatchers(HttpMethod.PUT, "/events/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PATCH, "events/**");
+                .antMatchers(HttpMethod.PATCH, "events/**").hasRole("ADMIN").and().csrf().disable();
     }
 }
