619f2257d0f405cbc4038e0df17c1f576520226e
MoMutawe1 <mutalogix@gmail.com>
2022-01-27 19:40:28 -0600

    Alter the default view of the ventRepository using Excerpts to apply projection to a collection of resources

--- src/main/java/com/springdatarest/eventmanagementapi/entities/projections/PartialEventProjection.java
+++ src/main/java/com/springdatarest/eventmanagementapi/entities/projections/PartialEventProjection.java
@@ -7,7 +7,7 @@ import java.time.Instant;
 import java.time.ZonedDateTime;
 
 @Projection(name="partial" ,types = {Event.class})
-public interface partialEventProjection {
+public interface PartialEventProjection {
     String getName();
     ZonedDateTime getStartTime();
     ZonedDateTime getEndTime();
--- src/main/java/com/springdatarest/eventmanagementapi/repos/EventRepository.java
+++ src/main/java/com/springdatarest/eventmanagementapi/repos/EventRepository.java
@@ -1,15 +1,17 @@
 package com.springdatarest.eventmanagementapi.repos;
 
 import com.springdatarest.eventmanagementapi.entities.Event;
+import com.springdatarest.eventmanagementapi.entities.projections.PartialEventProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZoneId;
 
-@Repository
+@RepositoryRestResource(excerptProjection = PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
     Page<Event> findByName(@Param("name")String name, Pageable pageable);
